2c7ad9b89aeb384bc61c21f4ae9425874cd5151a
Luis Paulo Sampaio Junior <luissampaio@hotmail.com>
2019-12-17 17:35:01 -0300

    Push

---
+++ Admin/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$("document").ready(function () {
+
+    var page = document.URL.split("/");
+    page = page[document.URL.split("/").length - 1];
+    if (page == "") {
+        $("#navPageHome").addClass("active");
+    }
+    else {
+        $(".nav-link").removeClass("active");
+        $("#navPage" + page).addClass("active");
+    }
+})
